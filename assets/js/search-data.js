d576d6c173e4e3cb212cf58ceeb447912c8ea2b5
marziehf <marzieh@cohere.com>
2025-11-12 11:13:51 +0000

    Deploying to gh-pages from @ marziehf/marziehf.github.io@3e322b4ca913fc23456b3c03a3197a1837bae71e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-talks",
-          title: "talks",
+          title: "Talks",
           description: "",
           section: "Navigation",
           handler: () => {
